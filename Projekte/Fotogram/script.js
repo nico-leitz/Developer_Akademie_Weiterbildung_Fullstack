1092c06226fc26cf300dce7f71d8102b6df82cf5
Nico <ndesk.ltrz@gmail.com>
2026-03-04 16:05:03 +0100

    feat: added modul array function

--- Projekte/Fotogram/script.js
+++ Projekte/Fotogram/script.js
@@ -3,9 +3,14 @@
 let modal = document.getElementById("dialog");
 let headlineH1 = document.getElementById("dialogTitle");
 let imgContainer = document.querySelector(".imgContainer");
+let generatedImage = document.querySelector(".generatedImage");
 let btnCloseModal = document.getElementById("btnCloseModal");
 let btnOpenModal = document.getElementById("btnOpenModal");
 let renderModalImg = document.getElementById("modalImg");
+let modalTitle = document.getElementById("modalTitle");
+let modalImage = document.getElementById("modalImage");
+let imageNumber = document.getElementById("imageNumber");
+let arrowLeft = document.getElementById("imgArrowLeft");
 
 let imageArray = [
   "1",
@@ -37,21 +42,43 @@ let imageTitle = [
   "A tree with snow",
 ];
 
-function renderImages() {
-  headlineH1.innerHTML = `Your Personal photo album`;
-  for (let i = 0; i < imageArray.length; i++) {
-    imgContainer.innerHTML += `
-    <img src="./assets/img/img${imageArray[i]}.svg" class="imgCointainer"/>`;
-  }
-}
-
-function renderImagesModal() {
-  openModal();
-}
+let imageAlt = [
+  "A picture of a glacier",
+  "Night City",
+  "Cloudy Sky",
+  "Birb png!",
+  "hurricane?",
+  "ice lake?",
+  "A swimming duck",
+  "man on boat!",
+  "small birb",
+  "cheeetaahh, but its a snow lion?",
+  "mountains with snow",
+  "A tree with snow",
+];
 
 // btnOpenModal.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
-imgContainer.addEventListener("click", openModal);
+arrowLeft.addEventListener("click", nextImage);
+
+function renderImages() {
+  headlineH1.innerHTML = `Your Personal photo album`;
+  for (let index = 0; index < imageArray.length; index++) {
+    imgContainer.innerHTML += `
+    <img src="./assets/img/img${imageArray[index]}.svg" class="generatedImage" onclick="renderDialog(${index})"/>`;
+  }
+}
+
+function renderDialog(index) {
+  openModal();
+
+  modalTitle.innerHTML = imageTitle[index];
+  modalImage.src = `./assets/img/img${imageArray[index]}.svg`;
+  modalImage.alt = imageAlt[index];
+  imageNumber.innerHTML = `${index}/12`;
+}
+
+function nextImage() {}
 
 function openModal() {
   modal.showModal();
