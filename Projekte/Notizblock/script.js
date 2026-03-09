9cb7ea6d9b0738cf96862fcd2c9afc9d9ee82f15
Nico <ndesk.ltrz@gmail.com>
2026-03-10 00:06:40 +0100

    feat: added addNotes() function und getTemplate function

--- Projekte/Notizblock/script.js
+++ Projekte/Notizblock/script.js
@@ -9,14 +9,36 @@ let notes = ["banane", "rasen mähen"];
 function renderNotes() {
   // -> ich muss definieren wo sie anzuzeigen sind
   let contentRef = document.getElementById("content");
+  contentRef.innerHTML = "";
 
   for (let indexNote = 0; indexNote < notes.length; indexNote++) {
     const note = " " + notes[indexNote];
-    contentRef.innerHTML += notes;
+    contentRef.innerHTML += getNoteTemplate(note);
   }
 }
 
+function getNoteTemplate(note) {
+  return `<p>+ ${note}</p>`;
+}
+
 // notizen hinzufügen
+
+function addNote() {
+  let userInputRef = document.getElementById("userInput");
+  let userInput = userInputRef.value;
+  notes.push(userInput);
+  userInputRef.value = "";
+
+  renderNotes();
+}
+
+// eingabe vom user defninieren
+
+// eingabe auslesen
+
+// eingabe speichern
+
+// eingabe anzeigen lassen
 
 // notizen löschen
 
