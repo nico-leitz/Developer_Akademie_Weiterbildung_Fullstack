9d7b5e1ae46c60f035d7632bd96bd4d67bc537fd
Nico <ndesk.ltrz@gmail.com>
2026-03-09 20:50:48 +0100

    updated javascript - breakpoint

--- Projekte/Notizblock/script.js
+++ Projekte/Notizblock/script.js
@@ -2,6 +2,20 @@
 
 // notizen anzeigen lassen
 
+let notes = ["banane", "rasen mähen"];
+
+// wann werden die notes angezeigt
+
+function renderNotes() {
+  // -> ich muss definieren wo sie anzuzeigen sind
+  let contentRef = document.getElementById("content");
+
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    const note = " " + notes[indexNote];
+    contentRef.innerHTML += notes;
+  }
+}
+
 // notizen hinzufügen
 
 // notizen löschen
